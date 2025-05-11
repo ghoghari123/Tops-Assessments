51a46f08870efe99034f6cd7ba7791f01265125b
hemanshi20 <158160795+ghoghari123@users.noreply.github.com>
2025-05-11 18:25:40 +0530

    Add files via upload

--- Assessment 4/Bank Management/banker.py
+++ Assessment 4/Bank Management/banker.py
@@ -1,4 +1,5 @@
 from datetime import datetime # It Get the correct date and time & it also store it with customer data or in logs.
+from file_handler import save_customer, log_trasaction
 
 customers = {} # create disctionary for customer, using to stores the records of all customer
 
@@ -7,37 +8,40 @@ def add_customer():
     acc_no = int(input("Enter Account Number : "))
     if acc_no in customers:
         print("Account Already Exists")
-        return
-    name = input("Enter Customer Name : ")
-    balance = float(input("Enter Opening Balance : "))
-    open_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    customers[acc_no] = {"Name":name,"Balace":balance,"Opening Date":open_date}
-    print("Recorded Inserted Successfully.....!")
+    else:
+        name = input("Enter Customer Name : ")
+        balance = float(input("Enter Opening Balance : "))
+        open_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        customers[acc_no] = {"Name":name,"Balace":balance,"Opening Date":open_date}
+        save_customer(customers)
+        log_trasaction(f"Add Customer : {acc_no} - {name}")
+        print("Recorded Inserted Successfully.....!")
     
 # then, view specific customer according to the user
 def viwe_customer():
     acc_no = input("Enter a account number to view : ").capitalize()
-    for acc_no,detail in customers.item():
-        if detail['acc_no'] == acc_no:
-            print(f"Account Number : {acc_no}\n Details : {customers[acc_no]}")
-        else:
-            print(f"We can't find this {acc_no} please chek the customer accounts..!")
+    if acc_no in customers:
+        print(f"Account Number : {acc_no}\n Details : {customers[acc_no]}")
+    else:
+        print(f"We can't find this {acc_no} please chek the customer accounts..!")
         
 # Search the customer
 def search_customer():
     search_name = input("Enter Customer Name : ")
+    status = False
     for acc_no,detail in customers.items():
         if detail['name'] == search_name:
-            print(f"Account Number : {acc_no}\n Details : {customers[detail]}")
-        else:
-            print(f"We can't find this {search_name} please chek the customer accounts..!")
+            print(f"Account Number : {acc_no}\n Details : {detail}")
+            status = True
+    if not status:
+        print(f"We can't find this {search_name} please chek the customer accounts..!")
         
 # View all customer
 def view_all_customer():
     if not customers:
         print("No Cutomer Record Found..!")
     for acc_no,detail in customers.items():
-        print(f"Account Number : {acc_no}\n Details : {customers[detail]}")
+        print(f"Account Number : {acc_no}\n Details : {detail}")
         
 # total amount in bank
 def total_amout():
@@ -65,9 +69,9 @@ def banker_menu():
         elif ch == '3':
             search_customer()
         elif ch == '4':
-            view_all_customer
+            view_all_customer()
         elif ch == '5':
-            total_amout()
+           total_amout()
         else:
-            print("Operation Invalid..!")
+            print("Invalid Choice")
 banker_menu()
--- Assessment 4/Bank Management/customer.py
+++ Assessment 4/Bank Management/customer.py
@@ -1,4 +1,44 @@
+from file_handler import save_customer,log_trasaction,load_customer
+
+# Withdrow Amount
+def withdrow_amount(customers):
+    acc_no = int(input("Enter Account Number : "))
+    if acc_no in customers:
+        amount = float(input("Enter Withdrow Amount : "))
+        if amount <= customers[acc_no]["balance"]:
+            customers[acc_no]["balance"] -= amount
+            save_customer(customers)
+            log_trasaction(f"{acc_no} Withdrow {amount}/-")
+            print("Withdrow Amount Successcully..!")
+        else:
+            print("Insufficient Balance..!")
+    else:
+        print("Account Not Found..!")
+
+
+# Deposite Amount
+def deposite_amount(customers):
+    acc_no = input("Enter Account Number : ")
+    if acc_no in customers:
+        amount = float(input("Enter Withdrow Amount : "))
+        customers[acc_no]["balance"] += amount
+        save_customer(customers)
+        log_trasaction(f"{acc_no} Withdrow {amount}/-")
+        print("Deposite Amount Successcully..!")
+    else:
+        print("Account Not Found..!")
+        
+        
+# View Balance
+def view_balance(customers):
+    acc_no = input("Enter Account Number : ")
+    if acc_no in customers:
+        print(f"Current Balance : {customers[acc_no]['balance']}/-")
+    else:
+        print("Account Not Found..!")
+
 def custer_menu():
+    customers = load_customer()
     print("Welcome To Customer's App")
     print("\n\nOperation Menu")
     print("Press 1 --> Withdraw Amount")
@@ -8,10 +48,11 @@ def custer_menu():
     ch = input("Enter Your Choice : ")
 
     if ch == '1':
-        pass
+        withdrow_amount(customers)
     elif ch == '2':
-        pass
+        deposite_amount(customers)
     elif ch == '3':
-        pass
+        view_balance(customers)
     else:
         print("Operation Invalid..!")
+custer_menu()
